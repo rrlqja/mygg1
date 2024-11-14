a62ad9692999220fbc81ccc20734c4be4969bcba
rlqja biz <rrlqja2@naver.com>
2024-11-14 10:59:36 +0900

    refactor: commonResponse 삭제

--- src/main/java/song/mygg/domain/common/controller/ExceptionController.java
+++ src/main/java/song/mygg/domain/common/controller/ExceptionController.java
@@ -1,27 +1,29 @@
 package song.mygg.domain.common.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import song.mygg.domain.common.exception.rest.RestException;
 import song.mygg.domain.common.exception.rest.RestNotFoundException;
-import song.mygg.domain.common.response.CommonResponse;
 
-import static org.springframework.http.HttpStatus.*;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionController {
     @ResponseBody
     @ExceptionHandler(RestException.class)
-    public CommonResponse<String> restException(RestException restException) {
+    public ResponseEntity<?> restException(RestException restException) {
         if (restException instanceof RestNotFoundException) {
-            return new CommonResponse<>(NOT_FOUND.value(), "not found", restException.getMessage());
-
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("message", "not found exception"));
         }
 
-        return new CommonResponse<>(BAD_REQUEST.value(), "rest exception", restException.getMessage());
+        return ResponseEntity.badRequest()
+                .body(Map.of("message", "rest exception"));
     }
 
 }
--- src/main/java/song/mygg/domain/jsph/controller/JSPHController.java
+++ src/main/java/song/mygg/domain/jsph/controller/JSPHController.java
@@ -3,12 +3,12 @@ package song.mygg.domain.jsph.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import song.mygg.domain.common.response.CommonResponse;
 import song.mygg.domain.jsph.entity.JSPHPost;
 import song.mygg.domain.jsph.service.JSPHService;
 
@@ -21,8 +21,8 @@ public class JSPHController {
 
     @ResponseBody
     @GetMapping("/{id}")
-    public CommonResponse<JSPHPost> getJSPHPostById(@PathVariable("id") Long id) {
+    public ResponseEntity<JSPHPost> getJSPHPostById(@PathVariable("id") Long id) {
         JSPHPost jsphPost = jsphService.getJSPHPostById(id);
-        return new CommonResponse<>(HttpStatus.OK.value(), "success", jsphPost);
+        return ResponseEntity.ok(jsphPost);
     }
 }
